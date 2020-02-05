a2d8cdcbffb096b22e630eb85c07848864ed0c73
Pilling <jaydnp@gmail.com>
2020-02-05 11:06:57 +1100

    add image tag to grid list

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,7 +16,7 @@ export interface Tile {
 export class AboutComponent implements OnInit {
 
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue', image: 'https://trello-attachments.s3.amazonaws.com/5d8aee7aa4124f73311be1b8/5e336d272f475c4bcffde96c/2928aeb07cfe3fafda9b152e2b01bd1b/nathan-anderson-kTaIjvHsyJg-unsplash.jpg'},
     {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
     {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
