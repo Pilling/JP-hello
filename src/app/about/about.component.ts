dd6c80660377139574062aac1bdc125ca34fb806
Pilling <jaydnp@gmail.com>
2020-02-12 06:59:19 +1100

    change grid list content

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,10 +16,10 @@ export interface Tile {
 export class AboutComponent implements OnInit {
 
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue', image: 'https://trello-attachments.s3.amazonaws.com/5d8aee7aa4124f73311be1b8/5e336d272f475c4bcffde96c/2928aeb07cfe3fafda9b152e2b01bd1b/nathan-anderson-kTaIjvHsyJg-unsplash.jpg'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Mountain', cols: 3, rows: 1, color: '#e6c229'},
+    {text: 'Writing', cols: 1, rows: 2, color: '#f17105'},
+    {text: 'Design/Software', cols: 1, rows: 1, color: '#6610f2'},
+    {text: 'Wandering', cols: 2, rows: 1, color: '#1a8fe3'},
   ];
 
   constructor() { }
